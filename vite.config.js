2aeeaf0ca7e9642f244d6d4d557f349d9c28af44
fahid2002 <fahid23105101323.diu.edu.bd>
2026-04-01 23:24:29 +0600

    19

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/DigiTools/',
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: process.env.NODE_ENV === 'production' && process.env.GITHUB_ACTIONS 
+      ? '/DigiTools/' 
+      : '/', 
+  }
 })
